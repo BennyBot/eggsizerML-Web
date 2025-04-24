7f020edc3ece5715a261275833c4b9cb108c4292
Benjamin Davis <davi1561@purdue.edu>
2025-04-24 10:15:53 -0400

    tofixed didnt work

--- src/App.jsx
+++ src/App.jsx
@@ -206,11 +206,11 @@ export default function EggSizerApp() {
       const maxLen = Math.max(otsuAreas.length, blobAreas.length);
 
       for (let i = 0; i < maxLen; ++i) {
-        oA = otsuAreas[i].toFixed(3) ?? null;
-        bA = blobAreas[i].toFixed(3) ?? null;
-        avgSize = ''
+        oA = otsuAreas[i] ?? null;
+        bA = blobAreas[i] ?? null;
+        avgSize = '';
         if (oA && bA) {
-          avgSize = ((oA + bA) / 2).toFixed(3);
+          avgSize = ((oA + bA) / 2);
         }
         rows.push({
           imgName   : file.name,
